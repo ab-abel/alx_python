19b648ceb9b728653b965fc732e2531d333bf41e
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-17 15:51:41 +0100

    fix task 3 negativevalue erros

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,12 +2,19 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-number = str(number)
-last_digit = int(number[-1])
-msg = "Last digit of {} is {}".format(number,last_digit)
-if last_digit>5:
-    print(msg, "and is greater than 5" )
-elif last_digit<6 and last_digit !=0:
+
+if(number > 0):
+    number_to_str = str(number)
+    last_digit = int(number_to_str[-1])
+elif(number < 0):
+    number_to_str = str(number)
+    last_digit = int('-'+number_to_str[-1])
+
+# print(last_digit)
+msg = str("Last digit of {} is {}".format(number, last_digit))
+if last_digit > 5:
+    print(msg, "and is greater than 5")
+elif last_digit < 6 and last_digit != 0:
     print(msg, "and is less than 6 and not 0")
-elif last_digit==0:
+elif last_digit == 0:
     print(msg, "and is 0")
