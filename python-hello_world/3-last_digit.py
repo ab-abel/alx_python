4f40c988cd763d947c50c285617549cc85676418
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-17 18:31:00 +0100

    fix validator issue

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,17 +3,16 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 
-#check if number is possitive 
+#b check if number is possitive 
 if(number > 0):
-    number_to_str = str(number) #convert the number to string
-    last_digit = int(number_to_str[-1]) #get the last digit
-elif(number < 0): #if negative 
-    number_to_str = str(number) #convert to string
-    last_digit = int('-'+number_to_str[-1]) #get the last digit
+    number_to_str = str(number) # convert the number to string
+    last_digit = int(number_to_str[-1]) # get the last digit
+elif(number < 0): # if negative 
+    number_to_str = str(number) # convert to string
+    last_digit = int('-' + number_to_str[-1]) # get the last digit
 else:
     last_digit = number
 
-# print(last_digit)
 msg = str("Last digit of {} is {}".format(number, last_digit))
 if last_digit > 5:
     print(msg, "and is greater than 5")
