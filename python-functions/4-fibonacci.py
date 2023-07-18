a4e021cd000b043315ac7e1a8c07b06df522f0bb
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-19 00:17:11 +0100

    task 4 fibonaci work through

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# fibonacci sequence
+# 0 1 1 2 3 5 8 13 21...
+
+def fib(n):
+    a = []
+    a.append(0)
+    a.append(1)  
+    for i in range(2, n):
+        a.append(a[i-1] + a[i-2])
+    print(a)
+
+
+def fib2(n):
+    c,d = 0,1
+    while (d<n):
+        print(d, end=', ')
+        c,d=d, c+d
+
+# fib2(20)
