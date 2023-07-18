46879a7fcf32becfa876cfb90f33e4d7995eaf35
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-18 19:08:16 +0100

    added the power function

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-# add = __import__('0-sum').add
+
 
 def add(i, j):
     return i + j
-
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
---
+++ python-functions/1-power.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# A function that raise a number to a given power
+def pow(a, b):
+    return a**b
