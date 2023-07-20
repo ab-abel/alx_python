65d1166aba66ba51f3e15965229f9dfa60c6052b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-20 10:20:10 +0100

    modify the function fib name

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # fibonacci sequence
 
+
 def fibonacci_sequence(n):
-    a = []
-    a.append(0)
-    a.append(1)  
-    for i in range(2, n):
-        a.append(a[i-1] + a[i-2])
-    print(a)
+    c,d = 0,1
+    while (d<n):
+        print(d, end=', ')
+        c,d=d, c+d
