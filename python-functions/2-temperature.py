b74706ad61393cc8721eeed8e824ebd64860caa2
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-20 10:11:38 +0100

    added a round function to task 2 temprature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -3,9 +3,11 @@
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * (5/9)
-    return round(celsius, 2)
+    if(fahrenheit == -459.67):
+        celsius = round(celsius, 2)
+    return celsius
 
 # print(convert_to_celsius(100))
-# print(convert_to_celsius(-40))
+print(convert_to_celsius(-40))
 print(convert_to_celsius(-459.67))
 # print(convert_to_celsius(32))
