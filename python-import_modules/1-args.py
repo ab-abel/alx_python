fd558ac8af575a9a25b1171d006f0721b19ccede
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-24 22:45:45 +0100

    correction based on checker feedback

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def list_args(*argv):
-    print("{}: arguments.".format(len(argv)))
-    for x in range(0, len(argv)):
-        result= "{}: {}".format(x+1, argv[x])
-        return result
+    argv_lenght = len(argv)
+    if argv_lenght == 1:    
+        print("{}: argument.".format(argv_lenght))
+    else:
+        print("{}: arguments.".format(argv_lenght))
+    for x in range(0, argv_lenght):
+            print("{}: {}".format(x+1, argv[x]))
         
-
 if __name__ == "__main__":
-    print(list_args())
+    list_args()
 
 # list_args("Hello", "Holberton", "School", "98", "Battery", "street")
