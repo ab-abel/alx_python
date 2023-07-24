7a15d6c532c05cd44406484dca459d109dd78113
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-24 20:36:16 +0100

    added PEP* validation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
     answer = "{} + {} = {}".format(a, b, add(a, b))
     print(answer)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
