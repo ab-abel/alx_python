1037c3dd42bc237f8037f370ce761c735f75cc5e
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-25 00:36:50 +0100

    hardest ever

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+import sys
 
 
 def list_args(*argv):
-    argv_lenght = len(argv)
+    
+   
+    argv_lenght = len(argv[0])
     if argv_lenght == 1:    
-        print("{} argument:".format(argv_lenght))
+        print("{} argument:".format(argv_lenght-1))
     elif argv_lenght == 0:
-        print("{} argument.".format(argv_lenght))
+        print("{} argument.".format(argv_lenght-1))
     else:
-        print("{}: arguments:".format(argv_lenght))
-    for x in range(0, argv_lenght):
-            print("{}: {}".format(x+1, argv[x]))
+        print("{}: arguments:".format(argv_lenght-1))
+    for x in range(1, argv_lenght):
+            print("{}: {}".format(x, argv[0][x]))
         
 if __name__ == "__main__":
-    list_args("Hello")
-    # list_args("Hello", "Holberton")
-    # list_args()
-    # list_args("98", "Battery", "street")
-    # list_args("98", "Battery", "street", "CA")
+    cmd_input = []
+    for i in range(0, len(sys.argv)):
+        cmd_input.append(str(sys.argv[i]))
+    list_args(cmd_input)
