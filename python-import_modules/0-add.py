ce5c1cf7cabae9761e96a6ccf11554ab1fd2d34b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-24 15:47:43 +0100

    added import command

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,9 +4,12 @@ def add(a, b):
     b = 2
     return(a+b)
 
-# print(add(1,3))
+# # print(add(1,3))
+
+def main():
+    print(add(1, 2))
 
 if __name__=="__main__":
-    print(add())
+    main()
     # import 0-add.py
 
