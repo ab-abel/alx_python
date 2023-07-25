d9b96b3245a0d373a3366e562a2e50b69d676084
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-25 08:08:23 +0100

    Catching a division by 0

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,9 +4,9 @@
 def safe_print_division(a, b):
     try:
         result = a/b
-    except ZeroDivisionError:
-        # result = "None"
         break
+    except ZeroDivisionError:
+        print("None")
     else:
         print("Inside result: {}".format(result))
     finally:
