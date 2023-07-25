b408792329d1934bd9ea1aeacfbfe37959a545bf
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-25 02:12:25 +0100

    task 3 exception handing try rxcept finally

---
+++ 3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        result = a/b
+
+    except ZeroDivisionError:
+        result = "None"
+    finally:
+        print("Inside result: ". format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    safe_print_division(12, 0)
