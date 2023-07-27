dc8734d952870eba0cc5b163a89ee98af32647ac
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-27 18:11:24 +0100

    message the using multiple return statment

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -8,7 +8,3 @@ def multiple_returns(sentence:str):
     else:
         first = sentence[0]
     return length, first
-
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
