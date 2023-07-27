eb15aa0534eb5881a0886b2e64caf34facf46c81
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-27 18:01:29 +0100

    task 1 still not ok

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,7 +4,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
-            # print(row)
-            print("{}".format(num), end=' ' if i < len(row)-1 else '$')
+            print("{:d}".format(num), end=' ' if i < len(row)-1 else '')
         print()
-    
