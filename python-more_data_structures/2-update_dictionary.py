34ef5a0bc956c2de197a43b9d235328bc38b0a5a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-28 00:22:50 +0100

    fixing sorted bug

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -10,8 +10,8 @@ def update_dictionary(a_dictionary, key, value):
             a_dictionary[key] = value        
         return a_dictionary
 
-
-def print_sorted_dictionary(a_dictionary):
-    sorted_dict = sorted(a_dictionary.items())
-    for k, v in sorted_dict.items():
-        print("{}: {}", format(k,v))
+def print_sorted_dictionary(my_dict):
+    """ Print sorted dictionary """
+    keys = sorted(my_dict.keys())
+    for k in keys:
+        print("{}: {}".format(k, my_dict[k]))
