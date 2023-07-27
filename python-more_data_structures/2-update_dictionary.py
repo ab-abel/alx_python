c24ff62ed05da8637e9bb7efd78c838ffc6ce5b0
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-27 23:29:37 +0100

    added the sorted part of dictionary function

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -14,8 +14,3 @@ def update_dictionary(a_dictionary, key, value):
 def print_sorted_dictionary(a_dictionary):
     keys = sorted(a_dictionary.keys())
     return keys
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print(print_sorted_dictionary(new_dict))
-
