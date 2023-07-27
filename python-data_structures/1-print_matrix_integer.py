575d7350b9153161e222667fc452166f7b29d3c1
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-27 15:50:59 +0100

    understanding the problem

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,20 +2,9 @@
 
 
 def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, num in enumerate(row):
+            # print(row)
+            print("{}".format(num), end=' ' if i < len(row)-1 else '$')
+        print()
     
-    new_mat = [num for elem in matrix for num in elem]
-    for i in range(3, len(new_mat) + 3, 4):
-        new_mat.insert(i, '$')
-        # print(i, end=" ")
-    if len(new_mat) == 0:
-        new_mat.append('$')
-    
-    print_2=""
-    for i in range(len(new_mat)):        
-        print("{}".format(new_mat[i]), end='')
-         
-vec = [[1,2,3], [4,5,6], [7,8,9]]
-
-print_matrix_integer(vec)
-print_matrix_integer("--")
-print_matrix_integer()
