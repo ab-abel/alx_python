f294f839af7771fe5e02f9e2929eee67e9eab831
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-31 20:54:43 +0100

    initial commit for the square module with exception

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,13 +9,7 @@ class Square:
     """
     def __init__(self, size=0):
         if type(size) != int:
-            try:
-                raise TypeError("size must be an integer")
-            except TypeError as te:
-                print(te)
+            raise TypeError("size must be an integer")
         if size<=0:
-            try:
-                raise ValueError("size must be >= 0")
-            except ValueError as var:
-                print(var)
+            raise ValueError("size must be >= 0")
         self.__size = size
