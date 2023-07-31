1449f1190818fc43c7656c861c489506d53de72a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-01 00:57:28 +0100

    fixing syntax error on size param

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,25 +9,34 @@ class Square:
     this class uses the getter to return the area from
     the square method
     """
-    def __init__(self, size=0):
-        self.__size = size
+    # size = 0
 
-    def size(self, value):
+    def __init__(self, size=0):
+        self.__size =0
+        self.set_size(size)
+        self.size = self.get_size()
+        
+    def set_size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value ** 2
+            self.__size = value
+            # self.size = value
+    
+    def get_size(self):
+        return self.__size
+
 
     def area(self):
-        size = self.__size
-        return size
+        area = self.__size ** 2
+        return area
 
 
 my_square = Square(89)
 print(my_square.size)
-# print(my_square.area())
-# my_square.size = 33
-# print(my_square.size)
-# print(my_square.area())
+print(my_square.area())
+my_square.size = 33
+print(my_square.size)
+print(my_square.area())
