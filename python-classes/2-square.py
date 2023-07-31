258d4a750ed5b5c7fbebb96539da5443288d188f
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-31 21:28:59 +0100

    adding class documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,6 +5,10 @@ Module calculate the area of a square
 
 
 class Square:
+    """
+    this class uses the getter to return the area from
+    the square method
+    """
     def __init__(self, size =0):
         if type(size) != int:
             raise TypeError("size must be an integer")
