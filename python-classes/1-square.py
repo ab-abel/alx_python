2217958d0f359a2f11632e4228fcb2b9936e4ccb
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-07-31 21:01:35 +0100

    initial commit for the square module with exception

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -13,7 +13,3 @@ class Square:
         if size<0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
