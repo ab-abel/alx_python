3eac2d52a3d39572ff8bf69580b0c8484fbb7508
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-01 07:09:22 +0100

    fixing syntax error

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,13 +9,12 @@ class Square:
     this class uses the getter to return the area from
     the square method
     """
-    # size = 0
 
     def __init__(self, size=0):
-        self.__size =0
+        self.__size = size
         self.set_size(size)
         self.size = self.get_size()
-        
+
     def set_size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
@@ -23,20 +22,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-            # self.size = value
-    
+
     def get_size(self):
         return self.__size
 
-
     def area(self):
-        area = self.__size ** 2
+        area = self.size
+        area = area ** 2
         return area
-
-
-my_square = Square(89)
-print(my_square.size)
-print(my_square.area())
-my_square.size = 33
-print(my_square.size)
-print(my_square.area())
