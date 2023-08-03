d5bd0a2581dc01cf9ea7da5a092ade5ecc0add6d
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 10:29:18 +0100

    task 2 increase documentation

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,7 +6,9 @@ a function that return true if class instance of or a subclass is the same
 
 def is_kind_of_class(obj, a_class):
     """
-    this method check if a class is an instance of the define object exam 1 is an instancce of the class int
+    this method check if a class is 
+    an instance of the define object 
+    exam 1 is an instancce of the class int
     the function is 
     """
     if issubclass(type(obj), a_class): #check two class
