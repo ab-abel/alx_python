8f46b48086e39dd96304b6237784ea98c4ee48d3
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 21:19:11 +0100

    modified substring

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -9,6 +9,11 @@ def is_kind_of_class(obj, a_class):
     Return the boolean True or False between the check 'obj' and 'a_class'.
     The issubclass methotd takes the obj type vairable, and a_class method.
     The if compirison operator is use to check if the return value is 'true or not'
+    >>> class ClassName(obj):
+            pass
+    >>> ClassName()
+    
+    >>> 
     """
     if issubclass(type(obj), a_class):
         return True
--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,7 +9,8 @@ class BaseGeometry:
     this method check if a class is an instance of the define object
     exam 1 is an instancce of the class int
     """
-    def __dir__(self):
+
+    def __dir__(cls):
 
         attrib = super().__dir__()
         n_attri = []
@@ -17,3 +18,7 @@ class BaseGeometry:
             if attr != '__init_subclass__':
                 n_attri.append(attr)
         return n_attri
+
+d = BaseGeometry()
+print(dir(d))
+print(dir(BaseGeometry))
