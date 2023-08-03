930f5e3adbf1bdc711c10470831d01ddc0ba2e4b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 10:40:21 +0100

    task 3 initial commit

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that return true if class instance of or a subclass is the same
+"""
+
+
+class BaseGeometry:
+    """
+    this method check if a class is an instance of the define object
+    exam 1 is an instancce of the class int
+    """
+    pass
