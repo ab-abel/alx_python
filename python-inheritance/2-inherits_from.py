5808f4f496a715585efd091e1e88b0e3142f2a0a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 21:31:48 +0100

    resolving doc string issues

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -9,11 +9,19 @@ def is_kind_of_class(obj, a_class):
     Return the boolean True or False between the check 'obj' and 'a_class'.
     The issubclass methotd takes the obj type vairable, and a_class method.
     The if compirison operator is use to check if the return value is 'true or not'
+    exampel:
     >>> class ClassName(obj):
-            pass
+    ...    pass
     >>> ClassName()
+    ...
+    >>>     pass
+    >>>     if issubclass(type(obj), a_class):
+    ...        return True
+    >>>     else:
+    ...         return False
     
-    >>> 
+    >>> obj = ClassName()
+    >>> is 
     """
     if issubclass(type(obj), a_class):
         return True
