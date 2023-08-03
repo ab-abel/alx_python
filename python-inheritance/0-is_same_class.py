1f991e78ad4e805cf9dac8187692e85b4766b5d6
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 09:27:19 +0100

    corrected instance of with is

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -3,12 +3,13 @@
 a function that return true if class instance is the same
 """
 
+
 def is_same_class(obj, a_class):
     """
     this method check if a class is an instance of the define object
     exam 1 is an instancce of the class int
     """
-    if isinstance(obj, a_class):
+    if type(a) is a_class:
         return True
     else:
         return False
