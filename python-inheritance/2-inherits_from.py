755f82f8acd1411a99f933181cad8bff0b68b8c0
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 10:11:57 +0100

    task 2 increase documentation

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -7,9 +7,9 @@ wise return false
 
 def is_kind_of_class(obj, a_class):
     """
-    this method check if a class is a subclass of the define object exam 1 is a subclass of the class int this method check 
-    if a class is a subclass of the define object
-    exam 1 is a subclass of the class int
+    this method check if a class is a issubclass of the define object exam 1 is a subclass of the class int this method check 
+    if a class is a issubclass of the define object
+    exam 1 is a issubclass of the class int 
     """
     if issubclass(type(obj), a_class):
         return True
