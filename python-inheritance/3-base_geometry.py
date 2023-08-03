a33d67018eab7a7c714ee5c72b613c51ff4a688a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-03 12:12:01 +0100

    task 3 added a magic method

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,4 +9,11 @@ class BaseGeometry:
     this method check if a class is an instance of the define object
     exam 1 is an instancce of the class int
     """
-    pass
+    def __dir__(self):
+
+        attrib = super().__dir__()
+        n_attri = []
+        for attr in attrib:
+            if attr != '__init_subclass__':
+                n_attri.append(attr)
+        return n_attri
