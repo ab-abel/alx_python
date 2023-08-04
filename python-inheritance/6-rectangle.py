d2848a765d8dc535fe92d5e2b0605aabc80d7e97
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 16:20:26 +0100

    task 6 initial commit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
+"""
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """
+    Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
+    """
     def __init__(self, width, height):
+        """
+        initialaizatio function for base geometry
+        """
         width = BaseGeometry.integer_validator(self, "width", width)
         height = BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
-    
