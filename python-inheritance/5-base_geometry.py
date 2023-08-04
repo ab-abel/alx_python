c8e6f93c2062baa06c7994c5f0a0a95001bfe118
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 15:37:38 +0100

    task 5 initial commit

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+#!/usr/bin/python3
+"""
+The module of a class name Geometry that has a single function
+"""
+
+
+class BaseGeometry:
+    """
+    this method check if a class is an instance of the define object
+    exam 1 is an instancce of the class int
+    """
+
+    def __dir__(cls) -> None:
+
+        attrib = super().__dir__()
+        n_attri = []
+        for attr in attrib:
+            if attr != '__init_subclass__':
+                n_attri.append(attr)
+        return n_attri
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name:str, value):
+        if type(value) != int:
+            raise ValueError("<name> must be an integer")
+        elif value <= 0:
+            raise ValueError("<name> must be greater than 0")
