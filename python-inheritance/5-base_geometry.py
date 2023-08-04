b67b70dc849e6a2374ef1f971d8878d6d52625c0
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 15:55:57 +0100

    task 5 adding task param

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -31,4 +31,4 @@ class BaseGeometry:
         if type(value) != int:
             raise TypeError("name must be an integer")
         elif value <= 0:
-            raise ValueError("name must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
