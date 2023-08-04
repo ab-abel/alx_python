a2aeb3c6aaead637890e9d1f7cf6c919c636f582
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 16:38:01 +0100

    intitial commit for task 8

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -5,17 +5,6 @@ Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
 BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    def __dir__(cls) -> None:
-        """
-        this method check if a class is an instance of the define object
-            exam 1 is an instancce of the class int
-        """
-        attrib = super().__dir__()
-        n_attri = []
-        for attr in attrib:
-            if attr != '__init_subclass__':
-                n_attri.append(attr)
-        return n_attri
     """
     Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
     """
