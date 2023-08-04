da2b8c3e982a9bd10c253ce29c65990b8c85c4e6
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 23:07:28 +0100

    finally learn how doc string works

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,23 +1,26 @@
 '''
 #!/usr/bin/python3
-Ths Module is issubclass method takes the obj type vairable, and a_class method.
+Ths Module is issubclass method takes the obj type vairable,
+and a_class method.
 '''
 
 
 def inherits_from(obj, a_class):
     '''
     Function to check if a data is a subclass of the other.
-    This function takes two numbers, 'obj' and 'a_class', and returns their sum.
+    This function takes two numbers, 'obj' and 'a_class',
+    and returns their sum.
 
         Parameters:
             obj (field): The first number.
             a_class (object): The second number.
 
         Returns:
-            bool (True and False): from the the comparison of obj and  a_class
+            bool (True and False): from the the comparison of
+            obj and a_class
 
     '''
-    if issubclass(type(obj), a_class):
+    if issubclass(obj, a_class):
         return True
     else:
         return False
