e9f5f7b88ed0a423d61665d4f56ed6bcb2ff8a85
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 23:42:30 +0100

    fixing isinstance issue

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -20,8 +20,8 @@ def inherits_from(obj, a_class):
             obj and a_class
 
     '''
-    # if isinstance(type(obj),  a_class):
-    if type(obj) is a_class and issubclass(type(obj), a_class):
+    # if isinstance(type(obj),  a_class): type(obj) is a_class 
+    if isinstance(type(obj),  a_class) or issubclass(type(obj), a_class):
         return True
     else:
         return False
