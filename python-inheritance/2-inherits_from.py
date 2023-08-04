b219f33fb6deafa7d44620397f811dbedae5f418
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 22:55:33 +0100

    back to the task 2 doc string

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,33 +1,25 @@
-"""
+'''
 #!/usr/bin/python3
 Ths Module is issubclass methotd takes the obj type vairable, and a_class method.
-"""
+'''
 
 
 def is_kind_of_class(obj, a_class):
-    """
+    '''
     Function to check if a data is a subclass of the other.
     This function takes two numbers, 'obj' and 'a_class', and returns their sum.
 
-    Parameters:
-    obj (int, float, boolean, string, list): The first number.
-    a_class (could be any class or object): The second number.
+        Parameters:
+            obj (field): The first number.
+            a_class (object): The second number.
 
-    Returns:
-    True: The output if the result of the subclass check is  true
-    False: If the result of the subclass is false
-    
-    """
+        Returns:
+            bool (True and False): from the the comparison of obj and  a_class
+
+    '''
     if issubclass(type(obj), a_class): 
-        """
-        Conditional statements
-        """
         return True
-        """
-        Conditional statements
-        """
     else:
-        """
-        Conditional statements
-        """
         return False
+
+print(__doc__)
