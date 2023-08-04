3e1490a33785324c2e43efdfb2f93d1cecf8e4d4
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 23:11:45 +0100

    finally learn how doc string works

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -20,7 +20,7 @@ def inherits_from(obj, a_class):
             obj and a_class
 
     '''
-    if issubclass(type(obj), type(a_class)):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
