7255fa12031b7d44ae59025d05e67c3e45915091
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-04 21:57:28 +0100

    use the str() method to convert the method object to string

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -16,9 +16,6 @@ class Rectangle(BaseGeometry, metaclass=BaseGeometryMetaClass):
     """
     Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py).
     """
-    def __dir__(cls)->None:
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
     def __init__(self, width, height):
         """
         initialaizatio function for base geometry
@@ -27,6 +24,12 @@ class Rectangle(BaseGeometry, metaclass=BaseGeometryMetaClass):
         BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
+    
+    def __str__(self):
+        return f"[retangle] {self.__width}/{self.__height}"
 
     def area(self):
         return self.__width/self.__height
+
+r = Rectangle(5, 5)
+print(r)
