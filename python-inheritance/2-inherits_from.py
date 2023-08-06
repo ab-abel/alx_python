51cb0e4a241da820f1934daed29b1c7cb800dd9b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-06 19:06:44 +0100

    correction for task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -23,6 +23,8 @@ def inherits_from(obj, a_class):
     # if isinstance(type(obj), a_class):
     if obj == True:
         return True
+    elif type(obj) == a_class:
+        return False
     else:
         return isinstance(type(obj), a_class) and issubclass(type(obj), a_class)and type(obj) != a_class
             
