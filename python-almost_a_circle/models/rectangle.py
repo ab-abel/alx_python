e05e262dc1a910c358c592688effeabd94a06744
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-07 12:48:40 +0100

    added attribute for width, height, x, y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,8 @@
     This is the first Rectangle that 
     inherits from Base Module
 '''
-from models.base import Base
+# from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -57,6 +58,23 @@ class Rectangle(Base):
         self._Rectangle__width = width
 
     @property
+    def width(self):
+        '''
+        Return:
+            return the width param from the private property
+        '''
+        return self._Rectangle__width
+    
+    @width.setter
+    def width(self, width):
+        '''
+        Return:
+           Set the width private property attributes field.
+        '''
+        self._Rectangle__width = width
+
+
+    @property
     def Rectangle__height(self):
         '''
         Return:
@@ -72,6 +90,21 @@ class Rectangle(Base):
         '''
         self._Rectangle__height = height
 
+    @property
+    def height(self):
+        '''
+        Return:
+            return the height param from the private property
+        '''
+        return self._Rectangle__width
+    
+    @height.setter
+    def height(self, width):
+        '''
+        Return:
+           Set the height private property attributes field.
+        '''
+        self._Rectangle__width = width
 
     @property
     def Rectangle__x(self):
@@ -90,6 +123,22 @@ class Rectangle(Base):
         self._Rectangle__x = x
 
     @property
+    def x(self):
+        '''
+        Return:
+            return the x param from the private property
+        '''
+        return self._Rectangle__width
+    
+    @x.setter
+    def x(self, width):
+        '''
+        Return:
+           Set the x private property attributes field.
+        '''
+        self._Rectangle__width = width
+
+    @property
     def Rectangle__y(self):
         '''
         Return:
@@ -104,3 +153,18 @@ class Rectangle(Base):
            Set the y private property attributes field.
         '''
         self._Rectangle__y = y
+    @property
+    def y(self):
+        '''
+        Return:
+            return the y param from the private property
+        '''
+        return self._Rectangle__width
+    
+    @y.setter
+    def y(self, width):
+        '''
+        Return:
+           Set the y private property attributes field.
+        '''
+        self._Rectangle__width = width
