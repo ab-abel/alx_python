108dd601feb174793d86ec18882baa67da3ba1b2
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-08 08:28:49 +0100

    initial commit for task 9

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+'''
+This class implement the square method based and also inherit from the 
+Rectanngle class we previously built
+'''
+
+# from rectangle import Rectangle
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''
+    this is a class that inherit from the rectangle class
+    '''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''
+        instantation of the square class
+        '''
+        super().__init__(width=size,height=size, x=x,y=y, id=id)
