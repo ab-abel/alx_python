3c474036e9106f75590c7db7ca4ca6e853dbc63e
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-09 22:54:11 +0100

    corrected task 0 with tabulation

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,12 +3,9 @@ This modele uses the requests moduleno  to
 fetch and get requests from  alu-intranet
 '''
 import requests
-# import json
+
 
 req = requests.get("https://alu-intranet.hbtn.io/status")
-
-
 print("Body response:")
-print("     - type: {}".format(type(req.text)))
-print("     - content: {}".format(req.text))
-# print(req.content)
+print("        - type: {}".format(type(req.text)))
+print("        - content: {}".format(req.text))
