df498bc3bdb8b5dad0a90974549709de3b6fb2c9
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-09 15:08:18 +0100

    modify response body

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,5 +3,11 @@ This modele uses the requests moduleno  to
 fetch and get requests from  alu-intranet
 '''
 import requests
+# import json
 
 req = requests.get("https://alu-intranet.hbtn.io/status")
+
+
+print("Body response:")
+print("- type: {}".format(type(req.text)))
+print("- content: {}".format(req.text))
