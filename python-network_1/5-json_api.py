22e6112ddc38558cbaab04a2f0e52b2a4e7bac76
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-11 00:35:27 +0100

    search task correction:

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -12,19 +12,12 @@ url = "http://0.0.0.0:5000/search_user"
 
 
 
-if str(sys.argv[1]) is None:
-    print("No result")
-else:
-    letter = str(sys.argv[1])
-    if letter:
-        q = letter
-    else:
-        q = ''
+data = {'q':str(sys.argv[1])}
     
-req = requests.post(url, q)
+req = requests.post(url, json=data)
 if req.status_code == 200:
-    result = req.json()
-    print(" {}".format(result))
+    result = req.text
+    print("{}".format(result))
 else:
     print("No result")
     
