5cca72207c6ee0f336905061c0835c09be33cc8b
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-10 10:25:15 +0100

    initial commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,18 @@
 '''
     This is a python script that takse URL and send a request to the URL,
     The value of this variable is different for each request.
+    
     Import:
         sys, requests
-    Paaramters:
-        url, gotten from sys
+
+    Parameters:
+
+        url - url of the requested page gotten from sys
+        url_to_str - return a str from the url list
+        req - get request to the specified url
+    
     Return:
+
         request headers x-request-Id
 '''
 import sys
