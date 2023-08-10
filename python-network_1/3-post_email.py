50fe70693e4d87a779114ff27d668ea7c76d7669
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-10 10:29:22 +0100

    remove if__main on task 3

--- python-network_1/3-post_email.py
+++ python-network_1/3-post_email.py
@@ -4,19 +4,14 @@ This is a python script that takse URL and send a request to the URL
 import sys
 import requests
 
-def post_email(email):
-    '''
-    this function takes a sys input and return the email body
-    '''
 
-    response = requests.post(email[1], email[2])
-    print("Your email is: {}".format(response.text))
+cmd_input = []
+for i in range(0, len(sys.argv)):
+    cmd_input.append(str(sys.argv[i]))
 
-    
+'''
+this function takes a sys input and return the email body
+'''
 
-if __name__ == "__main__":
-    cmd_input = []
-    for i in range(0, len(sys.argv)):
-        cmd_input.append(str(sys.argv[i]))
-    post_email(cmd_input)
-
+response = requests.post(cmd_input[1], cmd_input[2])
+print("Your email is: {}".format(response.text))
