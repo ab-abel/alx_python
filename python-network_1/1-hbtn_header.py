ecfc4ed1526e504567eba10460311bafb8836200
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-10 10:12:05 +0100

    remove the _if main

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,13 +4,11 @@ This is a python script that takse URL and send a request to the URL
 import sys
 import requests
 
-def request_url(url):
-    '''
-    request Url and return the headers X_request-id
-    '''
-    url = str(sys.argv[1])
-    request_url(url)
+'''
+request Url and return the headers X_request-id
+'''
 
-    url_to_str = "{}".format(url)
-    req = requests.get(url_to_str)
-    print(req.headers["X-Request-Id"])
+url = str(sys.argv[1])
+url_to_str = "{}".format(url)
+req = requests.get(url_to_str)
+print(req.headers["X-Request-Id"])
