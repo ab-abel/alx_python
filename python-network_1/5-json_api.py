9bc84082b58f12a36055a6fb67ba9433bb731e0f
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-11 12:02:46 +0100

    search task correction:

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -19,7 +19,7 @@ req = requests.post(url, data=post_data)
 if req.status_code == 200:
     
     data = '{}'.format(req.text)
-    print(data)
+
     # Remove curly braces and split the string
     pairs = data[1:-2].split(',')
 
