6b5ba1b28d7b659f8e45778b26850a23fecf9cd7
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-11 12:09:25 +0100

    search task correction:

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -10,28 +10,31 @@ request Url and return the headers X_request-id
 '''
 url = "http://0.0.0.0:5000/search_user"
 
-if (sys.argv[1]):
-    post_data ={'q':sys.argv[1]}
-else: 
-    post_data = {'q':''}
+try:
+    if (sys.argv[1]):
+        post_data ={'q':sys.argv[1]}
+    else: 
+        post_data = {'q':''}
 
-req = requests.post(url, data=post_data)
-if req.status_code == 200:
-    
-    data = '{}'.format(req.text)
+    req = requests.post(url, data=post_data)
+    if req.status_code == 200:
+        
+        data = '{}'.format(req.text)
 
-    # Remove curly braces and split the string
-    pairs = data[1:-2].split(',')
+        # Remove curly braces and split the string
+        pairs = data[1:-2].split(',')
 
-    # Create a dictionary from the key-value pairs
-    result = {}
-    for pair in pairs:
-        key, value = pair.split(':')
-        result[key.strip('"')] = value.strip('"')
-    
-    
-    print(f"[{result['id']}] {result['name']}")
+        # Create a dictionary from the key-value pairs
+        result = {}
+        for pair in pairs:
+            key, value = pair.split(':')
+            result[key.strip('"')] = value.strip('"')
+        
+        
+        print(f"[{result['id']}] {result['name']}")
 
-else:
+    else:
+        print("No result")
+        
+except IndexError:
     print("No result")
-    
