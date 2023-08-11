f66644d96181b1d617083cf359802071853904a8
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-11 01:14:20 +0100

    search task correction:

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -17,7 +17,7 @@ url = "http://0.0.0.0:5000/search_user"
 # }
     
 req = requests.post(url, str(sys.argv[1]))
-print(req.headers)
+print(req.content)
 # if req.status_code == 200:
 #     result = req.text
 #     print("{}".format(result))
