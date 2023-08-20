04730529501594da20a2ab4f0bf4835c4bf459b5
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-20 22:52:58 +0100

    PEP 8 validation

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,13 @@ A Script that list all users in the table
 import MySQLdb
 import sys
 
-# try connection and execution 
+# try connection and execution
 try:
     # connect to database
-    
+
     if len(sys.argv) > 3:
-    
-        database = MySQLdb.connect(user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}", db=f"{sys.argv[3]}")
+        database = MySQLdb.connect(user=f"{sys.argv[1]}", 
+                    passwd=f"{sys.argv[2]}", db=f"{sys.argv[3]}")
 
         # set cursor if connection succed
         cursor = database.cursor()
@@ -26,11 +26,11 @@ try:
         # loop through the result to get the state id and name
         for row in rows:
             print(row)
-    else: 
+    else:
         pass
 
-#   if there is an error catch it with and exception message
+    # if there is an error catch it with and exception message
 except MySQLdb.OperationalError as e:
-    
+
     # print the error message
     print("Connection failed. {}".format(e))
