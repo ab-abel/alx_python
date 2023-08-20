88bd1ae25acadd207ba65fe569723c5992e04213
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-20 21:06:49 +0100

    initial commit for object relational mapping

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+'''
+A Script that list all users in the table
+'''
+
+# import dbobject
+import MySQLdb
+
+# try connection and execution 
+try:
+    # connect to database
+    database = MySQLdb.connect(host='localhost', user="root", passwd="root", db="country")
+
+    # set cursor if connection succed
+    cursor = database.cursor()
+
+    # run the select statement on the states table
+    cursor.execute("SELECT * FROM states")
+
+    # fetch all rows in the result
+    rows = cursor.fetchall()
+
+    # loop through the result to get the state id and name
+    for row in rows:
+        print(row)
+
+#   if there is an error catch it with and exception message
+except MySQLdb.OperationalError as e:
+    
+    # print the error message
+    print("Connection failed. {}".format(e))
