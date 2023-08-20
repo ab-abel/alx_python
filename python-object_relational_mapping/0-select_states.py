ab029474f35d2ea8c4cd3d33c1e8967fa07ca45d
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-20 22:49:41 +0100

    added sys module

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,30 @@ A Script that list all users in the table
 
 # import dbobject
 import MySQLdb
+import sys
 
 # try connection and execution 
 try:
     # connect to database
-    database = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
+    
+    if len(sys.argv) > 3:
+    
+        database = MySQLdb.connect(user=f"{sys.argv[1]}", passwd=f"{sys.argv[2]}", db=f"{sys.argv[3]}")
 
-    # set cursor if connection succed
-    cursor = database.cursor()
+        # set cursor if connection succed
+        cursor = database.cursor()
 
-    # run the select statement on the states table
-    cursor.execute("SELECT * FROM states ORDER by states.id")
+        # run the select statement on the states table
+        cursor.execute("SELECT * FROM states ORDER by states.id")
 
-    # fetch all rows in the result
-    rows = cursor.fetchall()
+        # fetch all rows in the result
+        rows = cursor.fetchall()
 
-    # loop through the result to get the state id and name
-    for row in rows:
-        print(row)
+        # loop through the result to get the state id and name
+        for row in rows:
+            print(row)
+    else: 
+        pass
 
 #   if there is an error catch it with and exception message
 except MySQLdb.OperationalError as e:
