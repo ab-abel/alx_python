e86d4f89b454efedb251431b325a0f375e1b5708
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-23 10:02:58 +0100

    task 4 initial comit

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,5 @@
 '''
-This modele is the same as before but this take the name
-argument which is passes from the terminal
-
+A Script that list all users in the table
 '''
 
 # import dbobject
@@ -11,6 +9,7 @@ import sys
 # try connection and execution
 try:
     # connect to database
+
     if len(sys.argv) > 3:
         database = MySQLdb.connect(user=f"{sys.argv[1]}",
                                    passwd=f"{sys.argv[2]}",
@@ -19,25 +18,19 @@ try:
         # set cursor if connection succed
         cursor = database.cursor()
 
-        # run the select statement on the states table
-        # get data from terminal
-        cursor.execute("SELECT * FROM states WHERE name = %s \
-                        ORDER by states.id", (sys.argv[4],))
+        # run the select statement on the cities table
+        cursor.execute("SELECT * FROM cities ORDER by cities.id")
 
         # fetch all rows in the result
         rows = cursor.fetchall()
 
-        # loop through the result to get the state id and name
+        # loop through the result to get the cities id and name
         for row in rows:
-            # print only the capital letter not the small letter
-            # print(row) if row[1][0] == 'N' else None
             print(row)
-            
-        database.close()
-
     else:
         pass
 
+    database.close()        
     # if there is an error catch it with and exception message
 except MySQLdb.OperationalError as e:
 
