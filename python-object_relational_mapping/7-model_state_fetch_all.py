61cb5410d3f960ab6902aad531dabffca59ef04a
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-25 15:40:28 +0100

    PEP *e error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -18,17 +18,17 @@ import sys
 from sqlalchemy.orm import sessionmaker
 
 # define Base class for table class inheritance
-try:     
+try:
     # create a DB engine connection
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+             .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     # create a session variable and bint the engine to it
     Session = sessionmaker(bind=engine)
 
-    # instatiate the session 
+    # instatiate the session
     session = Session()
-    
+
     results = session.query(State).all()
 
     # print(result)
@@ -36,10 +36,8 @@ try:
     for result in results:
         print(f"{result.id}: {result.name}")
 
-except  AttributeError as e:
+except AttributeError as e:
     print(f"error message {e}")
 
 except sqlalchemy.exc.ProgrammingError as e:
-    print (f"An Error occured: {e}")
-
-
+    print(f"An Error occured: {e}")
