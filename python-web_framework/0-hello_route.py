55c3d94f84d3c43a081135db3416a4231985d82e
abel <ab.bell205@gmail.com>
2023-08-28 23:43:44 +0100

    added function doc string

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -7,15 +7,20 @@ from flask import Flask
 
 # define a flask instance
 app = Flask(__name__)
-# set strict_s;ashes flag to fals
-# for every route
-app.url_map.strict_slashes = False
+# set strict_s;ashes flag to false
 
 # create a route for index
 @app.route('/')
 # a function for
 def hello():
+    '''
+    a function definition that return
+    a strin hello... on a get request 
+    to a router page
+    '''
     return "Hello HBNB!"
 
 if __name__!='__main__':
+    # for every route
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0')
