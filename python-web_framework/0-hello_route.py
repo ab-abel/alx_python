046b1b0ad07d19140ade822f714faafecc4a06fb
abel <ab.bell205@gmail.com>
2023-08-28 23:19:44 +0100

    initial commit for python frame work

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,8 +1,18 @@
+'''
+Write a script that starts a Flask web application:
+'''
+# import flask module
 from flask import Flask
 
+
+# define a flask instance
 app = Flask(__name__)
+# set strict_s;ashes flag to fals
+# for every route
 app.url_map.strict_slashes = False
 
+# create a route for index
 @app.route('/')
+# a function for
 def hello():
     return "Hello HBNB!"
