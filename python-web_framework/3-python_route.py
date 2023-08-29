bb990384f12de2914bcb6a3505f79a40c0a99b16
Abel Enagbonmhena <ab.bell205@gmail.com>
2023-08-29 14:09:56 +0100

    initial commit for task 3

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,72 @@
+'''
+Write a script that starts a 
+Flask web application: The application
+must run on port the default port 
+and must run on 0.0.0.0
+'''
+# import flask module
+from glob import escape
+from flask import Flask
+
+
+# define a flask instance
+app = Flask(__name__)
+# set strict_s;ashes flag to false
+
+# create a route for index
+@app.route('/')
+# a function for
+def hello():
+    '''
+    a function definition that return
+    a strin hello... on a get request 
+    to a router page
+    param: 
+        type: NONE
+    Return:
+        type: String
+    '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    '''
+    a function definition that return
+    a strin hello... on a get request 
+    to a router page
+    param: 
+        type: NONE
+    Return:
+        type: String
+    '''
+    return "HBNB"
+
+@app.route('/c/<text>')
+def hbnb_C_is_fun(text):
+    '''
+    a function definition that return
+    a strin hello... on a get request 
+    to a router page
+    param: 
+        type: type
+    Return:
+        type: String
+    '''
+    return f"C {text.replace('_', ' ')}"
+
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    '''
+    this return a default text is cool
+    if no param is passed
+    param:
+        type: string
+    Return:
+        type: string
+    '''
+    return f"Python {text.replace('_', ' ')}"
+
+if __name__=='__main__':
+    # for every route
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0')
