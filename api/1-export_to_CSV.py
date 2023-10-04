aa10a54ce402191ed7f330e087a67be68eccfcad
ab-abel <ab.bell205@gmail.com>
2023-10-05 00:30:41 +0100

    fixing file creation erro

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -11,11 +11,12 @@ todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
 req = requests.get(todo_url) 
 results = json.loads(req.text)
 
-file = open(f"{sys.argv[1]}.csv", 'x')
 
-for result in results:
-    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
-    user = json.loads(users.text)
-    with open(f'{result['userId']}.csv', 'w', newline='') as file:
+    
+    for result in results:
+        users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(result['userId']))
+        user = json.loads(users.text)
+        
+        open(f"{result['userId']}.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([result['userId'], user['username'], result['completed'], result['title']])
