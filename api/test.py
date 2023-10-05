8d2bd9cadf0803718abcfe7bc1799e2d1b686713
ab-abel <ab.bell205@gmail.com>
2023-10-05 16:16:01 +0100

    added json

--- api/test.py
+++ api/test.py
@@ -54,3 +54,41 @@ def data_to_json(user_id):
 
 # function call
 data_to_json(sys.argv[1])
+
+
+
+'''
+A python script that uses the REST API for a given employee ID
+returns information about his/her TODO list progress.
+'''
+
+import json
+import requests
+import sys
+
+if __name__=='__main__':
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
+
+    name_info = pjson['username']
+
+    filename = "{}.json".format(employee_id)
+
+    # Create the dictionary structure
+    result = {
+        employee_id: [{
+            "task": item["title"],
+            "completed": item["completed"],
+            "username": name_info
+        } for item in pjson1]
+    }
+
+    # export data to json file
+
+    with open(filename, "w") as outfile:
+        json.dump(result, outfile)
