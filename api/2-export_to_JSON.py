0d1185ac2cb139c34a8e0f9ebc1c5470e81bcae2
ab-abel <ab.bell205@gmail.com>
2023-10-05 12:04:22 +0100

    define data_to_json function

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,41 +7,44 @@ import json
 import requests
 import sys
 
-# get input from user
-user_id = str(sys.argv[1])
+def data_to_json(user_id ):
+    """
+        Fetches the employee's details and TODO list using the provided API endpoints,
+        calculates the progress, and exports the information to a JSON file.
 
-# format url with input from user
-user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+        Parameters:
+        - employee_id (int): The ID of the employee.
 
-# request data frm todo and user api
-user_data = requests.get(user_url).json()
-todo_data = requests.get(todo_url).json()
+        Returns:
+        None
+    """
+    # format url with input from user
+    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
 
-# creat a json data from the todo and user object
-json_data = {
-    user_id :[
-        {
-            "task":task['title'],
-            "completed":task['completed'],
-            "username":user_data['username'],
-        }
-        for task in todo_data
-    ]
-}
+    # request data frm todo and user api
+    user_data = requests.get(user_url).json()
+    todo_data = requests.get(todo_url).json()
 
-# Write to JSON file name filename
-filename = f"{user_id}.json"
+    # creat a json data from the todo and user object
+    json_data = {
+        user_id :[
+            {
+                "task":task['title'],
+                "completed":task['completed'],
+                "username":user_data['username'],
+            }
+            for task in todo_data
+        ]
+    }
 
-# open the file and overwrite it content with w
-def write_to_json(json_data):
-    '''
-    a function that write a given json data to a json file
-        param : json data
-        return : none
-    '''
+    # Write to JSON file name filename
+    filename = f"{user_id}.json"
+
+    # open the file and overwrite it content with w
+
     with open(filename, 'w') as file:
         json.dump(json_data, file, indent=2)
 
 # function call 
-write_to_json(json_data)
+data_to_json(sys.argv[1])
