aa366e1b19b7d2ac4a7ccb774a1197cd9a39d901
ab-abel <ab.bell205@gmail.com>
2023-10-05 11:48:04 +0100

    added doc strings for task

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,35 +1,38 @@
 '''
 This module export content from an api into
-a csv file defined on creation
-    return : csv
+a JSON file defined on creation
+    filename: USER_ID.json
 '''
-
 import json
 import requests
 import sys
 
+# get input from user
 user_id = str(sys.argv[1])
 
+# format url with input from user
 user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
 todo_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
 
+# request data frm todo and user api
 user_data = requests.get(user_url).json()
 todo_data = requests.get(todo_url).json()
 
-filename = "{}.csv".format(user_id)
-
+# creat a json data from the todo and user object
 json_data = {
-        "user_id" :[
-            {
+    user_id :[
+        {
             "task":task['title'],
             "completed":task['completed'],
             "username":user_data['username'],
-            }
-            for task in todo_data
+        }
+        for task in todo_data
     ]
 }
 
-# Write to JSON file
+# Write to JSON file name filename
 filename = f"{user_id}.json"
+
+# open the file and overwrite it content with w
 with open(filename, 'w') as file:
     json.dump(json_data, file, indent=2)
