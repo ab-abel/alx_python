6f411cea02fd2aac9e3dfa3391ae1cbd29da7bbd
ab-abel <ab.bell205@gmail.com>
2023-10-05 15:34:34 +0100

    modify doc string

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,28 +7,29 @@ import json
 import requests
 import sys
 
-employee_id = sys.argv[1]
-api_request = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
-api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
-data = api_request.text
-pjson = json.loads(data)
-data1 = api_request1.text
-pjson1 = json.loads(data1)
+if __name__=='__main__':
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/%7B%7D/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
 
-name_info = pjson['username']
+    name_info = pjson['username']
 
-filename = "{}.json".format(employee_id)
+    filename = "{}.json".format(employee_id)
 
-# Create the dictionary structure
-result = {
-    employee_id: [{
-        "task": item["title"],
-        "completed": item["completed"],
-        "username": name_info
-    } for item in pjson1]
-}
+    # Create the dictionary structure
+    result = {
+        employee_id: [{
+            "task": item["title"],
+            "completed": item["completed"],
+            "username": name_info
+        } for item in pjson1]
+    }
 
-# export data to json file
+    # export data to json file
 
-with open(filename, "w") as outfile:
-    json.dump(result, outfile)
+    with open(filename, "w") as outfile:
+        json.dump(result, outfile)
