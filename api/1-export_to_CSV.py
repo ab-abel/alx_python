dbf03864288e2e1c4f5d8337fdffcb22d56bee9b
ab-abel <ab.bell205@gmail.com>
2023-10-05 07:32:24 +0100

    fixing file creation erro

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,19 +8,21 @@ import sys
 
 todo_url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}/todos'
 
-req = requests.get(todo_url) 
-results = json.loads(req.text)
+try:
+    csv_data = []
+    user_id = 0
+    req = requests.get(todo_url) 
+    results = json.loads(req.text)
+    for result in results:
+        users = requests.get(f"https://jsonplaceholder.typicode.com/users/{result['userId']}")
+        user = json.loads(users.text)
+        csv_data.append([result['userId'], user['username'], result['completed'], result['title']]) 
+        user_id = result['userId']
 
-csv_data = []
-user_id = 0
-for result in results:
-    users = requests.get(f"https://jsonplaceholder.typicode.com/users/{result['userId']}")
-    user = json.loads(users.text)
-    csv_data.append([result['userId'], user['username'], result['completed'], result['title']]) 
-    user_id = result['userId']
+    filename = f"{str(user_id)}.csv"
+    with open(filename, 'w', newline='') as file:
+        writer = csv.writer(file)
 
-filename = f"{user_id}.csv"
-with open(filename, 'w', newline='') as file:
-    writer = csv.writer(file)
-
-        # writer.writerow([result['userId'], user['username'], result['completed'], result['title']])
+            # writer.writerow([result['userId'], user['username'], result['completed'], result['title']])
+except TimeoutError:
+    print("connection timed out")
